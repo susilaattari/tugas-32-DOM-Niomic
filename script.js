88075969d63e32a0e39c7d59e292af963d26f662
susilaattari <susilanew@gmail.com>
2022-07-07 05:51:11 +0700

    tugas32 js Niomic DOM-Js

---
+++ script.js
@@ -0,0 +1,9 @@
+function jalankan(){
+    const namaDepan = document.getElementById('nama-depan');
+    const namaBelakang = document.getElementById('nama-belakang');
+    const hobi = document.getElementById('hobi');
+
+    console.log(namaDepan)
+    console.log(namaBelakang)
+    console.log(hobi)
+}
